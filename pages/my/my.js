cee1875690c8db460a350cb9f644bdae9685e7b9
Joush <joush970918@sina.com>
2020-06-21 10:40:00 +0800

    second commit

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,18 +1,58 @@
 // pages/my/my.js
+var httpsApi = "https://www.rexjoush.com";
+var app = getApp();
 Page({
+
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    unlogin: true,
+    logined: false,
+    // avatarUrl:
+    // nickName:
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    wx.checkSession({
+      success () {
+        //session_key 未过期，并且在本生命周期一直有效
+        that.setData({
+          unlogin: false,
+          logined: true,
+        })
+      },
+      fail () {
+        // session_key 已经失效，需要重新执行登录流程
+        console.log("fail");
+        this.login() //重新登录
+      }
+    })
+  },
 
+  login: function () {
+    console.log("aa");
+    wx.login({
+      success (res) {
+        if (res.code) {
+          //发起网络请求
+          // wx.request({
+          //   url: httpsApi,
+          //   data: {
+          //     code: res.code
+          //   }
+          // })
+          console.log(res.code);
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
   },
 
   /**
